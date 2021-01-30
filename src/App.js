0e3a4ff217a51b540f526c4704bce62f3e8f2f36
Kevin Tarwater <tarwaterk@gmail.com>
2021-01-30 16:00:37 -0700

    Update to test CI integration

--- src/App.js
+++ src/App.js
@@ -3,7 +3,7 @@ import './App.css';
 function App() {
   return (
     <div className="App">
-      Kevin Tarwater Portfolio CI Test
+      Kevin Tarwater Portfolio CI Test with build
     </div>
   );
 }
