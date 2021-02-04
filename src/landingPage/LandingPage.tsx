cac58344d2b0fc905a82b656435ba270fbb1bd00
tarwaterk <tarwaterk@gmail.com>
2021-02-03 19:37:15 -0700

    Add background styling for header

--- src/landingPage/LandingPage.tsx
+++ src/landingPage/LandingPage.tsx
@@ -1,12 +1,10 @@
 import React from 'react';
 import { Header } from '../header/Header';
-import { Navbar } from '../navbar/Navbar';
 import { Resume } from '../resume/Resume';
 
 export const LandingPage:React.FC = () => {
     return (
         <>
-            <Navbar />
             <Header />
             <Resume />
         </>
