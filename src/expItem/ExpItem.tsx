1f123f7d3e7cee479339ea280ce3bf3eb8b06d9b
tarwaterk <tarwaterk@gmail.com>
2021-02-07 15:10:05 -0700

    Style and populate experience

--- src/expItem/ExpItem.tsx
+++ src/expItem/ExpItem.tsx
@@ -1,16 +1,44 @@
 import React from 'react';
 import './expItem.css';
 
-export const ExpItem:React.FC = () => {
+interface Props {
+    reverse?: boolean;
+    employer: string;
+    title: string;
+    startDate: string;
+    endDate: string;
+    bullets: string[];
+}
+
+export const ExpItem:React.FC<Props> = ({
+    reverse,
+    employer,
+    title,
+    startDate,
+    endDate,
+    bullets
+}) => {
+    const imageBackground = {
+        backgroundImage: "linear-gradient(225deg, rgba(47,150,57,.8), rgba(0,255,0,0) 70.71%), linear-gradient(45deg, rgba(47,150,130,.8), rgba(0,255,0,0) 70.71%)"
+    }
+    const expItemDirection = {
+        flexDirection: reverse ? "row-reverse" as "row-reverse" : "row" as "row"
+    };
     return (
-        <div className={'expItem'}>
+        <div className={'expItem'} style={expItemDirection}>
             <div className={'expItem__bullet-list'}>
-                <div>Company Name</div>
-                <div>Job Title</div>
-                <div>Date Range</div>
-                <div>bulleted list of experience</div>
+                <div>{employer}</div>
+                <div>{title}</div>
+                <div>{`${startDate} - ${endDate || 'Present'}`}</div>
+                <ul>
+                    {
+                        bullets.map((item) => {
+                            return <li>{item}</li>;
+                        })
+                    }
+                </ul>
             </div>
-            <div className={'expItem__image'}></div>
+            <div className={'expItem__image'} style={imageBackground}></div>
         </div>
     );
 };
--- src/resume/Resume.tsx
+++ src/resume/Resume.tsx
@@ -1,13 +1,27 @@
 import React from 'react';
 import { ExpItem } from '../expItem/ExpItem';
 import './resume.css';
+import { expItems } from '../constants/resume';
 
 export const Resume:React.FC = () => {
     return (
         <>
-            <div className={'resume__intro-summary'}>SUMMARY</div>
+            <div className={'resume__intro-summary'}>Software engineer with 5 years experience mostly in front-end development.</div>
             <div className={'resume__experience-list'}>
-                <ExpItem />
+                {
+                    expItems.map((item) => {
+                        return (
+                            <ExpItem
+                                reverse={item.reverse}
+                                employer={item.employer}
+                                title={item.title}
+                                startDate={item.startDate}
+                                endDate={item.endDate}
+                                bullets={item.bullets}
+                            />
+                        );
+                    })
+                }
             </div>
         </>
     );
