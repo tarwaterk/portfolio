3885c63d8efcc383919b7ffac8be2fa6fe59b5e0
tarwaterk <tarwaterk@gmail.com>
2021-02-07 15:10:44 -0700

    Add resume constants

---
+++ src/constants/resume.ts
@@ -0,0 +1,58 @@
+export const expItems = [
+    {
+        reverse: false,
+        employer: 'Goldman Sachs',
+        title: 'Software Engineer (Vice President)',
+        startDate: '2018-07-01',
+        endDate: '',
+        bullets: []
+    },
+    {
+        reverse: true,
+        employer: 'Caleres',
+        title: 'Web Developer II',
+        startDate: '2016-04-01',
+        endDate: '2018-07-01',
+        bullets: [
+            'Translate Photoshop and Sketch designs to maintainable, responsive, and cross-browser compatible code using HTML, CSS, and Javascript.',
+            'Estimate scope and timelines of projects by collaborating with marketing and design.',
+            'Promote continual process improvement through documentation, standards, and automation of repetitive tasks.',
+            'Developed email building application using ReactJS and Express, which allows marketing to handle email campaigns resulting in quicker turn around of assets and development resources being free for higher ROI projects.',
+            'Develop cross-client compatible marketing emails.',
+            'Develop Salesforce email templates using HTML, CSS, and AMPscript allowing for personalization of automated marketing emails and fostering increased conversion.',
+            'Developed HTML generator for creating marketing assets (sitewide banners) using ReactJS saving resources across marketing, design, and development departments.'
+        ]
+    },
+    {
+        reverse: false,
+        employer: 'Caleres',
+        title: 'Enterprise Support II',
+        startDate: '2015-03-01',
+        endDate: '2016-04-01',
+        bullets: [
+            'A help desk position supporting Point of Sale, Cisco Routers, Broadband connections, wireless devices, printers and proprietary software at 1250 retail locations. The position also provides support for SAP, Microsoft Office, VPN, PC and virtual desktop environments to the corporate office and distribution centers.'
+        ]
+    },
+    {
+        reverse: true,
+        employer: 'Retail Technology Group',
+        title: 'Support Analyst III',
+        startDate: '2013-12-01',
+        endDate: '2015-03-01',
+        bullets: [
+            'Provided solutions to Point-of-Sale hardware and software issues and was point of escalation for 4 clients to maintain service level agreements.'
+        ]
+    },
+    {
+        reverse: false,
+        employer: 'University of Missouri',
+        title: 'Teaching Assistant',
+        startDate: '2009-08-01',
+        endDate: '2013-07-01',
+        bullets: [
+            'Developed immediate solutions to on-site equipment malfunctions.',
+            'Adapted lab procedures and questions to create more effective teaching methods through a committee.',
+            'Clarified physical concepts to facilitate student learning.'
+        ]
+    }
+]
