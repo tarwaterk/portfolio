d364b27811cdc1887d40f345c4753f913bb107d2
Kevin Tarwater <tarwaterk@gmail.com>
2021-02-20 14:44:46 -0700

    Implement color adjuster (correct) (#8)
    * Create color adjuster and apply result to all background
    
    * style DesignWidget to separate from header style
    
    * Fix hex conversion bug and adjust starting style
    
    * Change angle input to slider

--- src/landingPage/LandingPage.tsx
+++ src/landingPage/LandingPage.tsx
@@ -1,12 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Header } from '../header/Header';
 import { Resume } from '../resume/Resume';
 
 export const LandingPage:React.FC = () => {
+    const [baseAngle, setBaseAngle] = useState(15);
+    const [baseColor, setBaseColor] = useState('#4400DD');
+
     return (
         <>
-            <Header />
-            <Resume />
+            <Header baseAngle={baseAngle} baseColor={baseColor} setBaseAngle={setBaseAngle} setBaseColor={setBaseColor} />
+            <Resume baseAngle={baseAngle} baseColor={baseColor} />
         </>
     );
 };
---
+++ src/utils/color.ts
@@ -0,0 +1,41 @@
+export const adjustColor = (origColor: string) => {
+    /*
+    takes in a hex string starting with '#', breaks the main part of the color into R, G, and B sections,
+    determines which section is smallest, add ~100 to that value, then return the new hex color value
+    */
+   let red = parseInt(origColor.slice(1, 3), 16);
+   let green = parseInt(origColor.slice(3, 5), 16);
+   let blue = parseInt(origColor.slice(5, 7), 16);
+   let smallestValue = Math.min(red, Math.min(green, blue));
+   
+   switch (smallestValue) {
+    case red:
+        red = (red + 100) % 256;
+        break;
+    case green:
+        green = (green + 100) % 256
+        break;
+    case blue:
+        blue = (blue + 100) % 256;
+        break;
+    default:
+        break;
+   }
+
+   return `#${convertNumToColorHex(red)}${convertNumToColorHex(green)}${convertNumToColorHex(blue)}`;
+};
+
+export const convertNumToColorHex = (num: number) => {
+    let hexString = num.toString(16);
+    if (hexString.length < 2) {
+        hexString = `${hexString}${hexString}`;
+    }
+    return hexString;
+};
+
+export const generateLinearGradient = (baseColor: string, baseAngle: number, multiple: number = 3) => {
+    const firstLg = `linear-gradient(${(180 + baseAngle) % 360}deg, rgba(255,255,255,.8), rgba(255,0,0,0) 40.71%)`;
+    const secondLg = `linear-gradient(${(270 + baseAngle) % 360}deg, ${baseColor}, rgba(0,255,0,0) 70.71%)`;
+    const thirdLg = `linear-gradient(${(90 + baseAngle) % 360}deg, ${adjustColor(baseColor)}, rgba(0,0,255,0) 70.71%)`;
+    return multiple === 2 ? `${secondLg}, ${thirdLg}` : `${firstLg}, ${secondLg}, ${thirdLg}`;
+};
