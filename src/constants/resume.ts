8d23268f2f0611b880fd6ca517e9bbc7ef3f2bc5
tarwaterk <tarwaterk@gmail.com>
2021-02-21 14:42:06 -0700

    Update resume information and style

--- src/constants/resume.ts
+++ src/constants/resume.ts
@@ -1,18 +1,41 @@
-export const expItems = [
+export const EXP_SUMMARY = 'Software engineer with 5 years experience. Achieve a Master of Science degree in Physics and self-taught in computer science and web development.' +
+                        ' Skilled in Communication, Teamwork, and Front-End Web Development.';
+
+export const EXP_TECH = [
+    'HTML5 / CSS3',
+    'Javascript / Typescript',
+    'ReactJS',
+    'Redux',
+    'Node / NPM / Express',
+    'Gulp / Webpack',
+    'Git / Subversion',
+    'Jasmine / Ensyme / Jest',
+    'Java (exp. via using for algorithms)',
+    'Python (exp. via "Learn Python the Hard Way")',
+    'C++ (exp. via limited coursework and scientific computing)'
+];
+
+export const EXP_ITEMS = [
     {
         reverse: false,
         employer: 'Goldman Sachs',
         title: 'Software Engineer (Vice President)',
-        startDate: '2018-07-01',
+        startDate: '2018/07',
         endDate: '',
-        bullets: []
+        bullets: [
+            'Brought codebase from ~20% test coverage to target of >90%.',
+            'Converted javascript codebase to typescript to prevent errors caused by loose typing.',
+            'Designed API and refactored UI code to facilitate performance by adhering to REST principles and utilizing a service layer for the React application allowing for clearer responsibility delegation and form auto-save functionality.',
+            'Work with stakeholders and UX to groom user stories down to technical stories with levels of effort that could fit inside of a development sprint.',
+            'Create reusable React components and handle asynchronous logic with redux-saga to implement user stories.'
+        ]
     },
     {
         reverse: true,
         employer: 'Caleres',
         title: 'Web Developer II',
-        startDate: '2016-04-01',
-        endDate: '2018-07-01',
+        startDate: '2016/04',
+        endDate: '2018/07',
         bullets: [
             'Translate Photoshop and Sketch designs to maintainable, responsive, and cross-browser compatible code using HTML, CSS, and Javascript.',
             'Estimate scope and timelines of projects by collaborating with marketing and design.',
@@ -27,8 +50,8 @@ export const expItems = [
         reverse: false,
         employer: 'Caleres',
         title: 'Enterprise Support II',
-        startDate: '2015-03-01',
-        endDate: '2016-04-01',
+        startDate: '2015/03',
+        endDate: '2016/04',
         bullets: [
             'A help desk position supporting Point of Sale, Cisco Routers, Broadband connections, wireless devices, printers and proprietary software at 1250 retail locations. The position also provides support for SAP, Microsoft Office, VPN, PC and virtual desktop environments to the corporate office and distribution centers.'
         ]
@@ -37,8 +60,8 @@ export const expItems = [
         reverse: true,
         employer: 'Retail Technology Group',
         title: 'Support Analyst III',
-        startDate: '2013-12-01',
-        endDate: '2015-03-01',
+        startDate: '2013/12',
+        endDate: '2015/03',
         bullets: [
             'Provided solutions to Point-of-Sale hardware and software issues and was point of escalation for 4 clients to maintain service level agreements.'
         ]
@@ -47,8 +70,8 @@ export const expItems = [
         reverse: false,
         employer: 'University of Missouri',
         title: 'Teaching Assistant',
-        startDate: '2009-08-01',
-        endDate: '2013-07-01',
+        startDate: '2009/08',
+        endDate: '2013/07',
         bullets: [
             'Developed immediate solutions to on-site equipment malfunctions.',
             'Adapted lab procedures and questions to create more effective teaching methods through a committee.',
