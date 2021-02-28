0594722f888f9dddc58eebf72452b7ea53bcb2b4
tarwaterk <tarwaterk@gmail.com>
2021-02-28 13:06:07 -0700

    Give each experience item a unique backgroundImage

--- src/resume/Resume.tsx
+++ src/resume/Resume.tsx
@@ -2,7 +2,7 @@ import React from 'react';
 import { ExpItem } from '../expItem/ExpItem';
 import './resume.css';
 import { EXP_SUMMARY, EXP_TECH, EXP_ITEMS } from '../constants/resume';
-import { generateLinearGradient } from '../utils/color/color';
+import { adjustColor, generateLinearGradient } from '../utils/color/color';
 import { DesignWidgetContext } from '../designPicker/designWidgetContext/DesignWidgetContext';
 
 export const Resume:React.FC = () => {
@@ -26,11 +26,14 @@ export const Resume:React.FC = () => {
             <DesignWidgetContext.Consumer>
                 {
                     dwContextValue => {
-                        const backgroundImage = generateLinearGradient(dwContextValue.baseColor, dwContextValue.baseAngle, 2);
+                        let baseColor = adjustColor(dwContextValue.baseColor);
                         return (
                             <div className={'resume__experience-list'}>
                                 {
                                     EXP_ITEMS.map((item, index) => {
+                                        const baseAngle = dwContextValue.baseAngle + ((index % 2) * 180);
+                                        baseColor = adjustColor(baseColor, 10);
+                                        const backgroundImage = generateLinearGradient(baseColor, baseAngle, 2);
                                         return (
                                             <ExpItem
                                                 key={`expItem-${index}`}
--- src/utils/color/color.ts
+++ src/utils/color/color.ts
@@ -1,9 +1,8 @@
-export const adjustColor = (origColor: string) => {
+export const adjustColor = (origColor: string, step: number = 100) => {
     /*
     takes in a hex string starting with '#', breaks the main part of the color into R, G, and B sections,
     determines which section is smallest, add ~100 to that value, then return the new hex color value
     */
-   const COLOR_INCR = 100;
    const MOD_BASE = 256;
    let red = parseInt(origColor.slice(1, 3), 16);
    let green = parseInt(origColor.slice(3, 5), 16);
@@ -12,13 +11,13 @@ export const adjustColor = (origColor: string) => {
    
    switch (smallestValue) {
     case red:
-        red = (red + COLOR_INCR) % MOD_BASE;
+        red = (red + step) % MOD_BASE;
         break;
     case green:
-        green = (green + COLOR_INCR) % MOD_BASE;
+        green = (green + step) % MOD_BASE;
         break;
     case blue:
-        blue = (blue + COLOR_INCR) % MOD_BASE;
+        blue = (blue + step) % MOD_BASE;
         break;
     default:
         break;
